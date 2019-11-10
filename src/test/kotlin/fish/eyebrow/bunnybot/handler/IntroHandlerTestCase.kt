212696f47b1ec74105b114a97e522124ea8c868a
Alexander Johnston <Aliics@hotmail.com>
2019-11-10 22:43:07 +1300

    Fix long/string inconsistency

--- src/test/kotlin/fish/eyebrow/bunnybot/handler/IntroHandlerTestCase.kt
+++ src/test/kotlin/fish/eyebrow/bunnybot/handler/IntroHandlerTestCase.kt
@@ -58,7 +58,7 @@ internal class IntroHandlerTestCase {
 
     @Test
     internal fun `should populate database with new intro when given brand new intro with all fields`() {
-        val expectedDiscordId = 48293L
+        val expectedDiscordId = "48293"
         val expectedName = "Alexander"
         val expectedAge = "20"
         val expectedPronouns = "he/him"
@@ -68,7 +68,7 @@ internal class IntroHandlerTestCase {
             messageContent = "!intro name=${expectedName},age=${expectedAge},pronouns=${expectedPronouns},extra=${expectedExtra}"
         )
         val actualResultSet = getFirstRowOfQuery()
-        assertEquals(expectedDiscordId, actualResultSet.getLong("discord_id"))
+        assertEquals(expectedDiscordId, actualResultSet.getString("discord_id"))
         assertEquals(expectedName, actualResultSet.getString("name"))
         assertEquals(expectedAge, actualResultSet.getString("age"))
         assertEquals(expectedPronouns, actualResultSet.getString("pronouns"))
@@ -78,12 +78,12 @@ internal class IntroHandlerTestCase {
 
     @Test
     internal fun `should populate database with new intro when given brand new intro with only required fields`() {
-        val expectedDiscordId = 88820L
+        val expectedDiscordId = "88820"
         val expectedName = "Alfred"
         val expectedAge = "44"
         whenHandlerIsInvokedAsAuthorWithMessage(expectedDiscordId, "!intro name=${expectedName},age=${expectedAge}")
         val actualResultSet = getFirstRowOfQuery()
-        assertEquals(expectedDiscordId, actualResultSet.getLong("discord_id"))
+        assertEquals(expectedDiscordId, actualResultSet.getString("discord_id"))
         assertEquals(expectedName, actualResultSet.getString("name"))
         assertEquals(expectedAge, actualResultSet.getString("age"))
         assertNull(actualResultSet.getString("pronouns"))
@@ -93,13 +93,13 @@ internal class IntroHandlerTestCase {
 
     @Test
     internal fun `should populate database with sanitised new intro when given brand new intro with macro-like strings`() {
-        val expectedDiscordId = 7777777L
+        val expectedDiscordId = "7777777"
         val expectedName = "Larissa"
         val expectedAge = "19"
         val expectedExtra = "hello :D"
         whenHandlerIsInvokedAsAuthorWithMessage(expectedDiscordId, "!intro name=${expectedName},age=${expectedAge},extra=${expectedExtra}")
         val actualResultSet = getFirstRowOfQuery()
-        assertEquals(expectedDiscordId, actualResultSet.getLong("discord_id"))
+        assertEquals(expectedDiscordId, actualResultSet.getString("discord_id"))
         assertEquals(expectedName, actualResultSet.getString("name"))
         assertEquals(expectedAge, actualResultSet.getString("age"))
         assertNull(actualResultSet.getString("pronouns"))
@@ -116,14 +116,14 @@ internal class IntroHandlerTestCase {
 
     @Test
     internal fun `should overwrite data in database when there is already an existing row`() {
-        val expectedDiscordId = 111L
+        val expectedDiscordId = "111"
         val expectedName = "Oliver"
         val expectedAge = "1"
         val expectedExtra = "pwes! big wuv! :carrot: :heart:"
         whenHandlerIsInvokedAsAuthorWithMessage(expectedDiscordId, "!intro name=${expectedName},age=${expectedAge},extra=pwes! :carrot:")
         whenHandlerIsInvokedAsAuthorWithMessage(expectedDiscordId, "!intro name=${expectedName},age=${expectedAge},extra=${expectedExtra}")
         val actualResultSet = getFirstRowOfQuery()
-        assertEquals(expectedDiscordId, actualResultSet.getLong("discord_id"))
+        assertEquals(expectedDiscordId, actualResultSet.getString("discord_id"))
         assertEquals(expectedName, actualResultSet.getString("name"))
         assertEquals(expectedAge, actualResultSet.getString("age"))
         assertNull(actualResultSet.getString("pronouns"))
@@ -137,7 +137,7 @@ internal class IntroHandlerTestCase {
         val expectedHumouringPrompt = "Nothing to be added to your intro!"
         val expectedFormatOfIntroHeader = "To add stuff do it in the following format. (_only name and age are required, any order_):"
         val expectedFormatOfIntro = "name=YOUR NAME,age=YOUR AGE,pronouns=YOUR PRONOUNS,extra=YOUR EXTRA NOTES"
-        whenHandlerIsInvokedAsAuthorWithMessage(33332L, "!intro")
+        whenHandlerIsInvokedAsAuthorWithMessage("33332", "!intro")
         verify { messageChannel.createMessage(capture(slot)) }
         val actualMessage = slot.captured
         assertTrue(actualMessage.contains(expectedHumouringPrompt))
@@ -145,7 +145,7 @@ internal class IntroHandlerTestCase {
         assertTrue(actualMessage.contains(expectedFormatOfIntro))
     }
 
-    private fun whenHandlerIsInvokedAsAuthorWithMessage(authorId: Long, messageContent: String) {
+    private fun whenHandlerIsInvokedAsAuthorWithMessage(authorId: String, messageContent: String) {
         every { message.content } returns Optional.of(messageContent)
         every { author.id } returns Snowflake.of(authorId)
         introHandler.accept(messageCreateEvent)
--- src/test/resources/create_intro_table.sql
+++ src/test/resources/create_intro_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE intro
 (
     id         SERIAL PRIMARY KEY,
-    discord_id BIGINT      NOT NULL,
+    discord_id VARCHAR(18) NOT NULL,
     name       VARCHAR(50) NOT NULL,
     age        VARCHAR(12) NOT NULL,
     pronouns   VARCHAR(20),
